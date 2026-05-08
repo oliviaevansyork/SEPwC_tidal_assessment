4303659b354eb8b528d627afc607d9316a581c35
Olivia Evans <pgt523@york.ac.uk>
2026-05-08 14:42:08 +0100

    Add skeleton with all required function stubs

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -1,67 +1,18 @@
-# import the modules we need
-import pandas as pd
-import datetime
+"""
+Tidal analysis
+Reads tidal gauge data, calculates tidal constituents and sea-level rise.
+"""
+
+import sys
 import os
+import argparse
+import glob
+import datetime
+
 import numpy as np
+import pandas as pd
+import scipy.stats
+import matplotlib.dates
 import uptide
 import pytz
-import math
-from scipy import stats
-import matplotlib.dates as mdates
-import argparse
 
-
-def read_tidal_data(filename):
-
-    return
-    
-def extract_single_year_remove_mean(year, data):
-
-    return 
-
-
-def extract_section_remove_mean(start, end, data):
-
-    return year_data
-
-
-def join_data(data1, data2):
-
-    return 
-
-def sea_level_rise(data):
-
-    return
-
-def tidal_analysis(data, constituents, start_datetime):
-
-    return
-
-def get_longest_contiguous_data(data):
-
-    return 
-
-
-def main(args_list=None):
-
-    parser = argparse.ArgumentParser(
-                     prog="UK Tidal analysis",
-                     description="Calculate tidal constiuents and RSL from tide gauge data",
-                     )
-
-    parser.add_argument("directory",
-                    help="the directory containing txt files with data")
-    parser.add_argument('-v', '--verbose',
-                    action='store_true',
-                    default=False,
-                    help="Print progress")
-
-    args = parser.parse_args(args_list)
-    dirname = args.directory
-    verbose = args.verbose
-
-    print("Add your code here to do things!")
-    
-
-if __name__ == '__main__':
-    main()
